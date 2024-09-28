26855529fde1722a6e663ba3b9142212fec8aac0
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-09-28 17:52:20 +0900

    [Bronze V] Title: 시험 성적, Time: 96 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -0,0 +1,14 @@
+const fs = require("fs")
+
+const score = Number(fs.readFileSync(0).toString());
+
+if (score >= 90)
+    console.log("A");
+else if (score >= 80)
+    console.log("B");
+else if (score >= 70)
+    console.log("C");
+else if (score >= 60)
+    console.log("D");
+else
+    console.log("F");
